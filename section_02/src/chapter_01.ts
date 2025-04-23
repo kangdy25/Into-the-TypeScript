acd764ea5c54c708be52af9a8fcaf8e5f1f47d85
Shineast <canfrog@naver.com>
2025-04-23 11:49:42 +0900

    Primitive Types & Literal Type

---
+++ section_02/src/chapter_01.ts
@@ -0,0 +1,32 @@
+// Number
+let num1: number = 1;
+let num2: number = -123;
+let num3: number = 0.123;
+let num4: number = -0.123;
+let num5: number = Infinity;
+let num6: number = -Infinity;
+let num7: number = NaN;
+
+// String
+let str1: string = "a";
+let str2: string = "hello";
+let str3: string = `world`;
+let str4: string = `hello ${num1}`;
+
+// Boolean
+let bool1: boolean = true;
+let bool2: boolean = false;
+
+// Null
+let null1: null = null;
+
+// Undefined
+let undefined1: undefined = undefined;
+
+// Literal
+let literal1: 50 = 50;
+let literal2: "str" = "str";
+let literal3: false = false;
+
+// StrictNullChecks를 통해 Null 할당
+// let a: number = null;
