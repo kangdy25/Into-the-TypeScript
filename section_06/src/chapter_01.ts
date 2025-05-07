770e1fb2ef955fd526a82937d5cc64970e1e7b25
Kangdy <canfrog@naver.com>
2025-05-07 16:21:53 +0900

    Class in TypeScript

---
+++ section_06/src/chapter_01.ts
@@ -0,0 +1,49 @@
+// 타입스크립트의 클래스
+
+class Employee {
+  // 필드
+  name: string;
+  age: number;
+  position: string; 
+
+  // 생성자
+  constructor(name: string, age: number, position: string) {
+    this.name = name;
+    this.age = age;
+    this.position = position;
+  }
+
+  // 메서드
+  work() {
+    console.log("일하기");
+  }
+}
+
+class Executive extends Employee {
+  // 필드
+  officeNumber: number;
+
+  // 생성자
+  constructor(name: string, age: number, position: string, officeNumber: number) {
+    super(name, age, position);
+    this.officeNumber = officeNumber;
+  }
+
+  // 메서드
+  work() {
+    console.log("난 일 안한다");
+  }
+}
+
+const employeeA = new Employee("동윤", 25, "developer");
+console.log(employeeA);
+
+const employeeB : Employee = {
+  name: "다비지",
+  age: 25,
+  position: "engineer",
+  work() {
+    console.log("일하는 척하면서 마크 서버 열기");
+  }
+}
+
